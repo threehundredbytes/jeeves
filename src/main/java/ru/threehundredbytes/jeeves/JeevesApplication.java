88d15ada1a0dd934a28261f28fbe26cfcb4d628f
Anton Kosarev <antonkosarev.dev@gmail.com>
2023-01-30 14:43:34 +0200

    Initial commit

---
+++ src/main/java/ru/threehundredbytes/jeeves/JeevesApplication.java
@@ -0,0 +1,13 @@
+package ru.threehundredbytes.jeeves;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JeevesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JeevesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/threehundredbytes/jeeves/JeevesApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.threehundredbytes.jeeves;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JeevesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
